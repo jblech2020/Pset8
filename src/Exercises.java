47e783972a10de5a6b8d0c27547d251d19b0947e
Jaedan Blechinger-Slocum <jblech2020@gmail.com>
2020-01-21 13:10:42 -0500

    everywhere

--- src/Exercises.java
+++ src/Exercises.java
@@ -97,6 +97,7 @@ public class Exercises {
 	}
 
 	public boolean increasing(int[] numbers) {
+		//Check
 		if (numbers == null || numbers.length < 3) {
 			return false;
 		}
@@ -122,9 +123,28 @@ public class Exercises {
 	}
 
 	public boolean everywhere(int[] numbers, int x) {
-		// write your code here
-
-		return false;	// default return value to ensure compilation
+		if (numbers == null || numbers.length < 1) {
+			return false;
+		}
+		boolean lastPos = false;
+		int hole = 0;
+		for (int i = 0; i < numbers.length; i++) {
+			if (numbers[i] == x) {
+				lastPos = true;
+				hole = 0;
+			} else {
+				if (i == 1 && lastPos == false) {
+					return false;
+				} else if (lastPos == false) {
+					hole++;
+				}
+				lastPos = false;
+			}
+			if (hole == 2) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	public boolean consecutive(int[] numbers) {
